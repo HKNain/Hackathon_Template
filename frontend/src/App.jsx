a27e5350144247904043421d480587f8e90035c6
richa chauadhary <richachaudharyan@gmail.com>
2025-07-01 11:46:47 +0530

    added photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Login from './pages/Authentication/Login'
+import SignUp from './pages/Authentication/SignUp'
 
 const App = () => {
   return (
-    <>
-    
-    </>
+    <div>
+      {/* <Login />  */}
+      {/* <SignUp /> */}
+    </div>
   )
 }
 
-export default App
+export default App;
